1a95562350a339f46532e2c15c4715c0dda2e4af
Charliekofg <charliekofg@hotmail.com>
2025-11-07 04:33:08 +0000

    working on Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,34 @@
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import React, { useEffect } from "react";
 
 export const Home = () => {
+	const slug = "Carlossan"
+	let[lista, setLista] = UseState([])
+
 
   const {store, dispatch} =useGlobalReducer()
+  const obtenerContactos = async () => {
+	try {
+		const responseAgenda = await fetch (`https://playground.4geeks.com/contact/agendas/${slug}`)
+		if (responseAgenda.status === 404) {
+			const responseUsuario = await fetch (`https://playground.4geeks.com/contact/agendas/${slug}`, {
+				method: "POST",
+				headers: {"Content-Type": "application/json"}
+			})
+			await responseUsuario.json()
+
+		}
+		const respuesta = await fetch (`https://playground.4geeks.com/contact/agendas/${slug}/contacts`)
+		const data = await respuesta.json()
+		setLista (data.contacts)
+		dispatch ({type:"get_contactos", payload:data.contacts})
+	} catch (error) {
+		console.log(error);
+			
+	}
+  }
+  useEffect(()=>{},[])
 
 	return (
 		<div className="text-center mt-5">
