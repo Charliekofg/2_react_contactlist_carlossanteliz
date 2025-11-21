d43e186c682ffecc6bd75bc9cb36742c1d7f9afa
Charliekofg <charliekofg@hotmail.com>
2025-11-21 05:19:46 +0000

    logic done_tbd:improve logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,58 +6,64 @@ import { Link } from "react-router-dom"
 
 export const Home = () => {
 	const slug = "Carlossan"
-	let[lista, setLista] = useState([])
+	let [lista, setLista] = useState([])
 
 
-  const {store, dispatch} =useGlobalReducer()
-  const obtenerContactos = async () => {
-	try {
-		const responseAgenda = await fetch (`https://playground.4geeks.com/contact/agendas/${slug}`)
-		if (responseAgenda.status === 404) {
-			const responseUsuario = await fetch (`https://playground.4geeks.com/contact/agendas/${slug}`, {
-				method: "POST",
-				headers: {"Content-Type": "application/json"}
-			})
-			await responseUsuario.json()
+	const { store, dispatch } = useGlobalReducer()
+	const obtenerContactos = async () => {
+		try {
+			const responseAgenda = await fetch(`https://playground.4geeks.com/contact/agendas/${slug}`)
+			if (responseAgenda.status === 404) {
+				const responseUsuario = await fetch(`https://playground.4geeks.com/contact/agendas/${slug}`, {
+					method: "POST",
+					headers: { "Content-Type": "application/json" }
+				})
+				await responseUsuario.json()
+
+			}
+			const respuesta = await fetch(`https://playground.4geeks.com/contact/agendas/${slug}/contacts`)
+			const data = await respuesta.json()
+			setLista(data.contacts)
+			dispatch({ type: "get_contactos", payload: data.contacts })
+		} catch (error) {
+			console.log(error);
 
 		}
-		const respuesta = await fetch (`https://playground.4geeks.com/contact/agendas/${slug}/contacts`)
-		const data = await respuesta.json()
-		setLista (data.contacts)
-		dispatch ({type:"get_contactos", payload:data.contacts})
-	} catch (error) {
-		console.log(error);
-			
 	}
-  }
-  useEffect(()=>{ obtenerContactos() },[])
+	useEffect(() => { obtenerContactos() }, [])
 
-  const handleDelete = async (id) => {
-	try {
-		const response = await fetch(
-			`https://playground.4geeks.com/contact/agendas/${slug}/contacts/${id}`,
-			{ method: "DELETE" }
-		);
+	const handleDelete = async (id) => {
+		try {
+			const response = await fetch(
+				`https://playground.4geeks.com/contact/agendas/${slug}/contacts/${id}`,
+				{ method: "DELETE" }
+			);
 
-		if (!response.ok) {
-			throw new Error("Error al eliminar el contacto");
+			if (!response.ok) {
+				throw new Error("Error al eliminar el contacto");
+			}
+
+			setLista(lista.filter(contact => contact.id !== id));
+			dispatch({ type: "delete_contact", payload: id });
+
+		} catch (error) {
+			console.error("Error eliminando el contacto")
 		}
-		
-		setLista(lista.filter(contact => contact.id !== id));
-		dispatch({ type: "delete_contact", payload: id });
-
-	}	catch (error) {
-		console.error("Error eliminando el contacto")
-	}
-  };
+	};
 
 	return (
 		<div className="text-center mt-5">
 			<h1>Agenda de {slug}</h1>
-			{lista.map((item)=>(
-				<Card informacion={item} key ={item.id} onDelete={handleDelete}/>
-			))}
-			
+			<div className="container">
+				<div className="row g-4">
+					{lista.map((item) => (
+						<div className="col-12 col-sm-6 col-md-4" key={item.id}>
+							<Card informacion={item} onDelete={handleDelete} />
+						</div>
+					))}
+				</div>
+			</div>
 		</div>
+
 	);
 }; 
