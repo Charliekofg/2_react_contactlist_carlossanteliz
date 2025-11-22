d1d914d5b3570f656c4b799abf10ebf906b9f3b3
Charliekofg <charliekofg@hotmail.com>
2025-11-22 19:28:38 +0000

    tbd: improve Layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Check the <a target="_blank" href="https://4geeks.com/docs/start/start-react-advanced">template documentation</a> <i className="fa-solid fa-file"></i> for help.
-		</p>
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+	<footer className="footer mt-auto py-3 text-center text-secondary">
+		<h4 className="custom-heartbeat">
+			Agrega o edita tus contactos de manera dinámica.
+		</h4>
+		<p className="text-dark custom-flip">
+			Made with <i className="fa fa-heart text-danger" /> by Carlos Santeliz
 		</p>
 	</footer>
 );
+
